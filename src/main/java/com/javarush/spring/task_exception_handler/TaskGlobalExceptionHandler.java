18de11efdcd732fa86b717b65360e244c075c03b
Jamila Guliyeva <djamila.guliyeva>
2023-04-03 19:05:54 +0400

    Exception Handler

--- src/main/java/com/javarush/spring/task_exception_handler/TaskGlobalExceptionHandler.java
+++ src/main/java/com/javarush/spring/task_exception_handler/TaskGlobalExceptionHandler.java
@@ -2,8 +2,10 @@ package com.javarush.spring.task_exception_handler;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@ControllerAdvice
 public class TaskGlobalExceptionHandler {
 
     @ExceptionHandler
